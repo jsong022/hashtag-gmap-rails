70cb00b16ded3bd6bf9d2ba5f8bcc276642b3d9a
Jay <jsong022@ucr.edu>
2016-05-20 04:42:31 -0700

    Implement dynamic search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
   def home
-    qry="SELECT * FROM rails_gmap.mytable WHERE location = 'Los Angeles, CA';"
-    @records = ActiveRecord::Base.connection.select_all(qry)
+  end
+
+  def search
+    location = params[:location]
+    if location == "" then redirect_to '/' end
+    qry="SELECT * FROM rails_gmap.mytable WHERE location = '"+location+"';"
+    @records = ActiveRecord::Base.connection.select_all(qry);
+    @location = location
   end
 end
