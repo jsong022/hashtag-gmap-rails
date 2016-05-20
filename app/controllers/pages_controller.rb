5a2f052ba67c622fef2a4941ba18b2f5972a6c18
Jay <jsong022@ucr.edu>
2016-05-20 04:46:32 -0700

    Handle some invalid inputs (redirect to homepage)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,7 @@ class PagesController < ApplicationController
     if location == "" then redirect_to '/' end
     qry="SELECT * FROM rails_gmap.mytable WHERE location = '"+location+"';"
     @records = ActiveRecord::Base.connection.select_all(qry);
+    if not @records[0] then redirect_to '/' end
     @location = location
   end
 end
